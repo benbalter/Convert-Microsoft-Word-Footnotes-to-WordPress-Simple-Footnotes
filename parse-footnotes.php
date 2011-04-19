75b4b0c2d62009703aa93a1a5eded861b04f7770
Benjamin J. Balter <ben@balter.com>
2011-04-19 10:47:00 -0800

    added plugin header

--- parse-footnotes.php
+++ parse-footnotes.php
@@ -1,6 +1,15 @@
 <?php
+/*
+Plugin Name: Convert Footnotes
+Plugin URI: http://ben.balter.com/2011/03/20/regular-expression-to-parse-word-style-footnotes/
+Description: Converts Word Footnotes to Simple Footnotes format. Requires Simple Footnotes installed, available at: http://wordpress.org/extend/plugins/simple-footnotes/
+Version: 0.1
+Author: Benjamin J. Balter
+Author URI: http://ben.balter.com/
+*/
+
 /**
- * Function which uses regular expression to parse Microsoft Word footnotes 
+ * Function which uses regular expression to parse Microsoft Word footnotes
  * into WordPress's Simple Footnotes format
  *
  * @param string $content post content from filter hook
@@ -9,29 +18,29 @@
  */
  
 function bb_parse_footnotes( $content ) {
+    
     global $post;
-
     if ( !isset( $post ) )
     	return;
-
+    	
     //if we have already parsed, kick
     if ( get_post_meta($post-&gt;ID, 'parsed_footnotes') )
     	return $content;
-
+    
     $content = stripslashes( $content );
-
+    
     //grab all the Word-style footnotes into an array
-    $pattern = '#\&lt;a href\=&quot;\#_ftnref([0-9]+)&quot;\&gt;\[([0-9]+)\]\&lt;/a\&gt; (.*)#';
+    $pattern = '#&lt;a href\=&quot;\#_ftnref([0-9]+)&quot;&gt;\[([0-9]+)\]&lt;/a&gt; (.*)#';
     preg_match_all( $pattern, $content, $footnotes, PREG_SET_ORDER);
     
     //build find and replace arrays
     foreach ($footnotes as $footnote) {
-    	$find[] = '#\&lt;a href\=&quot;\#_ftn'.$footnote[1].'&quot;\&gt;\['.$footnote[1].'\]\&lt;/a\&gt;#';
+    	$find[] = '#&lt;a href\=&quot;\#_ftn'.$footnote[1].'&quot;&gt;\['.$footnote[1].'\]&lt;/a&gt;#';
     	$replace[] = '[[ref]' . str_replace( array(&quot;\r\n&quot;, &quot;\r&quot;, &quot;\n&quot;), &quot;&quot;, $footnote[3]) . '[/ref]]';
     }
     
     //remove all the original footnotes when done
-    $find[] = '#\&lt;div\&gt;\s*&lt;a href\=&quot;\#_ftnref([0-9]+)&quot;\&gt;\[([0-9]+)\]\&lt;/a\&gt; (.*)\s*\&lt;/div\&gt;\s+#';
+    $find[] = '#&lt;div&gt;\s*&lt;a href\=&quot;\#_ftnref([0-9]+)&quot;&gt;\[([0-9]+)\]&lt;/a&gt; (.*)\s*&lt;/div&gt;\s+#';
     $replace[] = '';
     
     //make the switch
